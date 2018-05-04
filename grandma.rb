bb4441e967049b38f95df32c0d191d273545cd47
jee <jag918@nyu.edu>
2018-05-04 12:36:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,9 @@
 def speak_to_grandma(words)
   if words == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN"
     "I LOVE YOU TOO PUMPKIN!" 
   elsif words.upcase ==words
-    puts "NO, NOT SINCE 1938!"
     "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP SONNY!"
     "HUH?! SPEAK UP, SONNY!"
   end
 end
